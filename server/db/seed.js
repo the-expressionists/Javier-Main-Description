68b45642d3d944944bd54a4db61d34eb157c6687
Joseph Toney <wumbabum@gmail.com>
2021-01-22 20:52:31 -0600

    updated seed.js to seed fake breadcrumb data

--- server/db/seed.js
+++ server/db/seed.js
@@ -1,25 +1,25 @@
 const mongoose = require('mongoose');
 const db = require('./db.js');
-let generateData = require('./faker/fakerGen.js');
+let generateFakes  = require('./faker/fakerGen.js');
 
 db.Item.deleteMany({}, () => {
   console.log('Emptied Items successful');
 })
+  .catch( err => {
+    console.log('Error deleting items: ' + err);
+  })
   .then( () => {
     return Promise.all(
-      generateData(100).map( itemProps => {
+      generateFakes(100).map( itemProps => {
         let item = new db.Item(itemProps);
         return item.save()
       })
     );
-  })
-  .catch( err => {
-    console.log('Error generating data: ' + err);
   })
   .then( () => {
     console.log(`Mongo Database seed successful`);
     return mongoose.disconnect()
   })
   .catch( err => {
-    console.log(err);
-  })
+    console.log('Error generating data: ' + err);
+  });
