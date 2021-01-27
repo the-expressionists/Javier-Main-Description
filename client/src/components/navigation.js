260bbad3a9240ee984f75547a50eba6749b5b3ca
Joseph Toney <wumbabum@gmail.com>
2021-01-26 22:37:38 -0600

    Styled the last child of navigation

--- client/src/components/navigation.js
+++ client/src/components/navigation.js
@@ -1,20 +1,38 @@
 import React from 'react';
 
-let Navigation = (props) => (
+let Navigation = (props) => {
+  let lastIndex = props.breadcrumbs.length - 1;
+
+  let navLink = (page, index) => {
+    if (index < props.breadcrumbs.length - 1) {
+      return (
+        <a href={page.url} className="nav-link">
+          <span className="hover">{page.name}</span>
+          <span className="space-around">›</span>
+        </a>
+      );
+    } else {
+      return (
+        <a href={page.url} id="nav-link-last" className="nav-link">
+          <span className="hover">{page.name}</span>
+        </a>
+      );
+    }
+  }
+
+  return (
     <div id="nav-container">
         <ol className="nav-list inline-block">
             {
               props.breadcrumbs.map( (page, index) => (
                 <li key={index} className="nav-item">
-                  <a href={page.url} className="nav-link">
-                    <span>{page.name}</span>
-                    <span className="space-around">›</span>
-                  </a>
+                  {navLink(page, index)}
                 </li>  
               ))
             }
         </ol>
     </div>
-);
+  )
+};
 
 export default Navigation;
