ab4f099dd8a17b2e8abce1bbabad31554388c3db
Joseph Toney <wumbabum@gmail.com>
2021-01-28 16:32:46 -0600

    added a new endpoint for one item without using ID

--- client/src/utils/requests.js
+++ client/src/utils/requests.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 // import { Module } from 'webpack';
 
 module.exports.findOne = (callback) => {
-    axios.get('http://localhost:3000/api/items/')
+    axios.get('http://localhost:3000/api/itemDetails/')
       .then(function ({data}) {
         callback(data[0]);
       })
