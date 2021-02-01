7ea4f8c4dcf32a5671c7a57f1dcafed4982e20b1
Joseph Toney <wumbabum@gmail.com>
2021-02-01 02:42:16 -0600

    fixed some typos/bugs

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
 const TerserPlugin = require('terser-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
 const BrotliPlugin = require('brotli-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
 const isDevelopment = true;
