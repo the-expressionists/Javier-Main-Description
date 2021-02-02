b9933e8939d132310b4a65ff6d077b47a7e0a563
Joseph Toney <wumbabum@gmail.com>
2021-02-02 11:47:42 -0600

    bugfix

--- client/src/components/navigation.js
+++ client/src/components/navigation.js
@@ -5,6 +5,9 @@ import NavLink from './navLink';
 let Navigation = (props) => (
   <div className={styles['nav-container']}>
       <ol className={`${styles['nav-list']} ${styles['inline-block']}`}>
+          <div className={styles['logo']}>
+            <img className={styles['logo-img']} src='https://i.imgur.com/gO9deqF.png'></img>
+          </div>
           {
             props.breadcrumbs.map( (page, index) => (
               <li key={index} className={styles['nav-item']}>
